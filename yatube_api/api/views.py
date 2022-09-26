1e4952b7a75c5541b84ec95398e2b26341f5de70
Alexander K <a.kazarin@mail.ru>
2022-09-26 18:00:20 +0300

    1st review

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import filters, mixins, pagination, permissions, viewsets
+from rest_framework import filters, mixins, permissions, viewsets
 
 from rest_framework.pagination import LimitOffsetPagination
 from django_filters.rest_framework import DjangoFilterBackend
